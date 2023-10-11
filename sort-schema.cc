ce185bdac394f4576c0eeba16c21a76ed2e92d01
Russell Wallace <russell.wallace@gmail.com>
2023-10-11 11:32:29 +0100

    lex

--- sort-schema.cc
+++ sort-schema.cc
@@ -13,7 +13,6 @@
 #include <iostream>
 #include <iterator>
 #include <ostream>
-#include <regex>
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -24,6 +23,132 @@ using namespace std;
 #else
 #define debug(a) cout << __FILE__ << ':' << __LINE__ << ": " << __func__ << ": " << #a << ": " << (a) << '\n'
 #endif
+
+string file;
+string text;
+
+void readText() {
+	ifstream is(file, std::ios::in);
+	text = {istreambuf_iterator<char>(is), istreambuf_iterator<char>()};
+
+	// make sure input ends with a newline, to simplify parser code
+	if (text.empty() || text.back() != '\n')
+		text += '\n';
+}
+
+bool isid(unsigned char c) {
+	return isalnum(c) || c == '_';
+}
+
+enum {
+	// SORT
+	t_char,
+	t_comment,
+	t_space,
+	t_word,
+};
+
+struct Tok {
+	int tag;
+	int i, j;
+
+	Tok(int tag, int i, int j): tag(tag), i(i), j(j) {
+	}
+};
+
+void lex() {
+	auto s = text.data();
+	int i = 0;
+	while (s[i]) {
+		auto j = i;
+		switch (s[i]) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+		case 'G':
+		case 'H':
+		case 'I':
+		case 'J':
+		case 'K':
+		case 'L':
+		case 'M':
+		case 'N':
+		case 'O':
+		case 'P':
+		case 'Q':
+		case 'R':
+		case 'S':
+		case 'T':
+		case 'U':
+		case 'V':
+		case 'W':
+		case 'X':
+		case 'Y':
+		case 'Z':
+		case '_':
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+		case 'g':
+		case 'h':
+		case 'i':
+		case 'j':
+		case 'k':
+		case 'l':
+		case 'm':
+		case 'n':
+		case 'o':
+		case 'p':
+		case 'q':
+		case 'r':
+		case 's':
+		case 't':
+		case 'u':
+		case 'v':
+		case 'w':
+		case 'x':
+		case 'y':
+		case 'z':
+			do
+				++j;
+			while (isid(s[j]));
+			toks.emplace_back(t_word, i, j);
+			i = j;
+			continue;
+		case '\'':
+			++src;
+			while (*src != '\'') {
+				switch (*src) {
+				case '\\':
+					src += 2;
+					continue;
+				case '\n':
+					err("unclosed quote");
+				}
+				++src;
+			}
+			++src;
+			tok.assign(src0, src);
+			return;
+		}
+	}
+}
 
 template <class T> void topologicalSortRecur(const vector<T>& v, vector<T>& o, unordered_set<T>& visited, T a) {
 	if (!visited.insert(a).second)
@@ -44,6 +169,7 @@ template <class T> void topologicalSort(vector<T>& v) {
 int main(int argc, char** argv) {
 	try {
 		bool inplace = 0;
+		vector<string> files;
 		for (int i = 1; i < argc; ++i) {
 			auto s = argv[i];
 			if (*s == '-') {
@@ -66,6 +192,12 @@ int main(int argc, char** argv) {
 				throw runtime_error(string(argv[i]) + ": unknown option");
 			}
 			files.push_back(s);
+		}
+
+		for (auto& file0: files) {
+			file = file0;
+			readText();
+			lex();
 		}
 		return 0;
 	} catch (exception& e) {
